3abc2b304448d1a2a074208c73f347ff8e3055b8
Martin Lucas-Smith <martin@lucas-smith.co.uk>
2014-04-11 22:31:14 +0100

    Use external templating library and clear out internal functions

--- classes/telluswhere.class.php
+++ classes/telluswhere.class.php
@@ -143,6 +143,7 @@ class telluswhere
 		
 		# Load required libraries
 		require_once ('application.php');
+		require_once ('templating.php');
 		
 		# Define the location of the stub launching file
 		$this->baseUrl = application::getBaseUrl ();
@@ -243,7 +244,7 @@ class telluswhere
 		$this->{$this->action} ();
 		
 		# Render the page
-		$html = $this->doTemplateSubstitution ($this->templateHtml, $this->template);
+		$html = templating::doTemplateSubstitution ($this->templateHtml, $this->template);
 		
 		# Show the HTML
 		echo $html;
@@ -368,10 +369,10 @@ class telluswhere
 		$page = '/404.html';
 		
 		# Get the template
-		$templateHtml = $this->convertDesignerHtmlToTemplate ($page);
+		$templateHtml = templating::convertDesignerHtmlToTemplate ($page, $this->styleDirectory, $this->replacedPlaceholders, $this->getStyleDirectory ('default'));
 		
 		# Get the HTML
-		$html = $this->doTemplateSubstitution ($templateHtml, $this->template);
+		$html = templating::doTemplateSubstitution ($templateHtml, $this->template);
 		
 		# Return the HTML
 		return $html;
@@ -388,163 +389,9 @@ class telluswhere
 		$templateLocation = $this->actions[$action]['url'] . (substr ($this->actions[$action]['url'], -1) == '/' ? 'index.html' : '');	// Convert /path/ to /path/index.html
 		
 		# Obtain the template
-		$html = $this->convertDesignerHtmlToTemplate ($templateLocation);
+		$html = templating::convertDesignerHtmlToTemplate ($templateLocation, $this->styleDirectory, $this->replacedPlaceholders, $this->getStyleDirectory ('default'));
 		
 		# Return the template HTML
-		return $html;
-	}
-	
-	
-	# Function to convert the designer's raw HTML to a templatised HTML page; this is a preprocessor which enables a template to be dropped in from a designer without making changes first
-	private function convertDesignerHtmlToTemplate ($page)
-	{
-		# Determine the location
-		$path = $this->styleDirectory . $page;
-		$file = $_SERVER['DOCUMENT_ROOT'] . $path;
-		
-		# If the file does not exist, fall back to the default (which will exist, as it is part of the repository)
-		if (!is_readable ($file)) {
-			$this->styleDirectory = $this->getStyleDirectory ('default');
-			$path = $this->styleDirectory . $page;
-			$file = $_SERVER['DOCUMENT_ROOT'] . $path;
-		}
-		
-		# Load the file
-		$html = file_get_contents ($file);
-		
-		# Chop trailing directory index; e.g. "/path/index.html" becomes "/path/"
-		$html = $this->htmlCleanChopDirectoryIndex ($html);
-		
-		# Make HTML paths absolute; e.g. "css/" becomes "/css/"
-		$html = $this->htmlCleanPathsAbsolute ($html, $path);
-		
-		# Replace templated sections with placeholders
-		$html = $this->commentsToPlaceholders ($html, $this->replacedPlaceholders);
-		
-		# Return the HTML
-		return $html;
-	}
-	
-	
-	# Function to chop directory indexes from links
-	private function htmlCleanChopDirectoryIndex ($html)
-	{
-		# Define directory index filename(s)
-		$supported = array ('index.html', );		// More can be added if found necessary, e.g. index.php
-		$supportedDirectoryIndexesRegexp = implode ('|', $supported);
-		
-		# HTML href links
-		$html = preg_replace ('@(\s+)(href)="(' . $supportedDirectoryIndexesRegexp . ')"@', '$1$2="./"', $html);			// Special case: href="index.html" becomes href="./"
-		$html = preg_replace ('@(\s+)(href)="([^"]*)/(' . $supportedDirectoryIndexesRegexp . ')"@', '$1$2="$3/"', $html);
-		
-		# Return the HTML
-		return $html;
-	}
-	
-	
-	# Function to rewrite HTML paths to be absolute
-	private function htmlCleanPathsAbsolute ($html, $path)
-	{
-		# Extract all URL references or return HTML as-is)
-		#!# No support for single-quoted (var='text') attributes quotes yet
-		#!# Will currently catch href="path"/src="path" appearing within the HTML as plain text
-		if (!preg_match_all ('@\s+(href|src)="([^"]+)"(\s|>)@', $html, $pathsOriginal, PREG_SET_ORDER)) {return $html;}
-		
-		# Determine the path prefix that needs to be inserted
-		$path = dirname ($path . '.bogus') . '/';	// .bogus ensures that dirname doesn't convert "/foo/bar" (which should not be supplied anyway) to /foo
-		$delimiter = '@';
-		$prefix = preg_replace ($delimiter . '^' . addcslashes ($this->styleDirectory, $delimiter) . $delimiter, '', $path);	// i.e. replace /style/default/ with /, leaving e.g. /contacts/
-		
-		# Work through each path to determine the replacement path for each match
-		$paths = array ();
-		foreach ($pathsOriginal as $i => $match) {
-			$paths[$i] = $match[2];		// By default, start with unamended original
-			
-			# Full URLs should be left unchanged
-			if (preg_match ('|^https?://.+$|', $paths[$i])) {continue;}
-			
-			# Absolute URLs should be left unchanged
-			if (preg_match ('|^/.+$|', $paths[$i])) {continue;}
-			
-			# Pure anchors should be left changed
-			if (preg_match ('|^#.*$|', $paths[$i])) {continue;}
-			
-			# Current-directory only URLs ( ./ or ./something ) should be substituted with the absolute equivalent
-			if ($paths[$i] == '.') {$paths[$i] = './';}	// Normalise
-			if (preg_match ('|^\./(.*)$|', $paths[$i], $matches)) {
-				$paths[$i] = $prefix . $matches[1];
-				continue;
-			}
-			
-			# Directory-traversal URLs - chop prefix for each, i.e. ../contacts/ => /prefix/../contacts/ => /contacts/
-			if ($paths[$i] == '..') {$paths[$i] = '../';}	// Normalise
-			if (preg_match ('|^\.\./(.*)$|', $paths[$i], $matches)) {
-				$newPrefix = $prefix;
-				while (preg_match ('|^\.\./(.*)$|', $paths[$i], $matches)) {
-					if (strlen ($newPrefix)) {	// Never traverse higher than / - if HTML of ../../ should have been ../ then treat it as such
-						$newPrefix = str_replace ('\\', '/', dirname ($newPrefix));	// Chop last component
-					}
-					$paths[$i] = $newPrefix . $matches[1];
-				}
-				continue;
-			}
-			
-			# Prefix remainder, which are "from here" paths, e.g. "path/to" becomes "/prefix/path/to"
-			$paths[$i] = $prefix . $paths[$i];
-			
-			#!# Not yet baseUrl -compliant
-		}
-		
-		# Construct the find/replace entry; $match[1] is href/src; $match[2] is the original path
-		$replacements = array ();
-		foreach ($pathsOriginal as $i => $match) {
-			$find    = sprintf (' %s="%s"', $match[1], $match[2]);
-			$replace = sprintf (' %s="%s"', $match[1], $paths[$i]);
-			$replacements[$find] = $replace;
-		}
-		
-		# Perform replacements
-		$html = strtr ($html, $replacements);
-		
-		# Return the HTML
-		return $html;
-	}
-	
-	
-	# Function to insert placeholders, by replacing comments in the HTML where the placeholders go; the aim here is that a designer can supply code with both sample HTML and the placeholders in
-	# This looks for "<!-- {$placeholdername} --> then lines of HTML here, then <!-- {/$placeholdername} -->"
-	private function commentsToPlaceholders ($html, &$replacedPlaceholders = array ())
-	{
-		# Cache matched placeholder comments; note \1 is a backreference to ensure the opening and closing tags match, and the s modifier enables multiple-line matches
-		$regexp = '|' . '<!--\s+\{\$([^}]+)\}\s+-->(.+)<!--\s+/\{\$\1\}\s+-->' . '|s';
-		if (preg_match_all ($regexp, $html, $matches, PREG_SET_ORDER)) {
-			foreach ($matches as $match) {
-				$replacedPlaceholders[$match[1]] = $match[2];		// placeholdername => html
-			}
-		}
-		
-		# Do the replacement of placeholder comments with actual placeholders
-		$html = preg_replace ($regexp, '{\$\1}', $html);
-		
-		# Return the HTML
-		return $html;
-	}
-	
-	
-	# Function to render the page
-	private function doTemplateSubstitution ($templateHtml, $replacements)
-	{
-		# Convert to Smarty-format placeholders
-		$substitutions = array ();
-		foreach ($replacements as $find => $replace) {
-			$find = '{$' . $find . '}';
-			$substitutions[$find] = $replace;
-		}
-		
-		# Perform substitutions
-		$html = strtr ($templateHtml, $substitutions);
-		
-		# Return the HTML
 		return $html;
 	}
 	
@@ -556,7 +403,7 @@ class telluswhere
 		$htmlBlock = $this->replacedPlaceholders[$placeholderName];
 		
 		# Extract the HTML between placeholder-comments nested within the form template to leave a standard template for the form
-		$template = $this->commentsToPlaceholders ($htmlBlock, $replacedPlaceholders);
+		$template = templating::commentsToPlaceholders ($htmlBlock, $replacedPlaceholders);
 		
 		# Convert each replaced placeholder to ultimateForm format
 		$replacements = array ();
@@ -572,7 +419,7 @@ class telluswhere
 		}
 		
 		# Substitute the placeholders for the ultimateForm placeholders
-		$templateUltimateFormFormat = $this->doTemplateSubstitution ($template, $replacements);
+		$templateUltimateFormFormat = templating::doTemplateSubstitution ($template, $replacements);
 		
 		# Strip <form> and </form> tags if present
 		$templateUltimateFormFormat = preg_replace ('/<form ([^>]+)>/s', '', $templateUltimateFormFormat);

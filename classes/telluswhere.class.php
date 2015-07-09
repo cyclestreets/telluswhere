cf58bf8233b161798acf804117577992885361ad
mvl22 <martin@lucas-smith.co.uk>
2015-07-09 08:39:11 +0100

    For now, limit Like facility to administrative users

--- classes/telluswhere.class.php
+++ classes/telluswhere.class.php
@@ -1123,6 +1123,9 @@ class telluswhere
 			table.compressed td {padding-top: 1px; padding-bottom: 1px;}
 		</style>
 		';
+		if (!$this->userIsBatchUploader) {
+			$html .= '<style type="text/css">#likes {display: none;}</style>';
+		}
 		if (!$viewOnlyMode) {
 			if (!$selectedIdData) {
 				$html .= "\n" . '<p id="helptext">Zoom all the way in, using +/- or mouse scroll functions, then click on the map to set the marker.</p>';

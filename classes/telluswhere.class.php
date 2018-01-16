cyclestreets/telluswhere

commit e0423f4f8279e0295561a8b8e5b86a1e0855d02b

In embed page, generalise category and add support for multiple categories

classes/telluswhere.class.php
@@ -720,8 +720,8 @@ class telluswhere
 		
 		# Define the embeddable map types
 		$types = array (
-			'suggest' => 'Suggested cycle parking',
-			'current' => 'Current cycle parking',
+			'suggest' => 'Suggested %categoryLabel',
+			'current' => 'Current %categoryLabel',
 		);
 		
 		# Determine the type selected, if any, throwing a 404 if invalid
@@ -737,11 +737,16 @@ class telluswhere
 		
 		# Show listing if no type selected
 		if (!$type) {
-			$list = array ();
-			foreach ($types as $type => $label) {
-				$list[] = "<a class=\"button color huge circle\" href=\"/{$type}/embed/\">{$label}</a>";
+			$linkGroups = array ();
+			foreach ($this->categories as $category) {
+				$list = array ();
+				foreach ($types as $type => $label) {
+					$label = str_replace ('%categoryLabel', $this->categoryLabels[$category], $label);
+					$list[] = "<a class=\"button color huge circle\" href=\"/{$type}/embed/\">{$label}</a>";
+				}
+				$linkGroups[] = "\n<p>" . implode ('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $list) . '</p>';
 			}
-			$this->template['links'] = "\n<p>" . implode ('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $list) . '</p>';
+			$this->template['links'] = implode ("\n<br /><br />", $linkGroups);
 			return true;
 		}
 		

commit b8972a9480a11e1577559ef4677bdf61f304a3f5

Fix label regression in e0423f4f8279e0295561a8b8e5b86a1e0855d02b

classes/telluswhere.class.php
@@ -771,7 +771,7 @@ class telluswhere
 			
 			# Construct the introduction HTML
 			$html .= "\n<p>Here, you can create a map widget to embed on your own website.</p>";
-			$html .= "\n<p>The map widget will show <strong>" . lcfirst ($types[$type]) . "</strong> [<a href=\"{$this->baseUrl}/embed/\">change?</a>].</p>";
+			$html .= "\n<p>The map widget will show <strong>" . lcfirst (str_replace ('%categoryLabel', $this->categoryLabels[$this->categories[0]], $types[$type])) . "</strong> [<a href=\"{$this->baseUrl}/embed/\">change?</a>].</p>";
 			$html .= "\n<p>To add it to your website:</p>";
 			$html .= "\n<style type=\"text/css\">
 				div.code {border: 1px solid #ddd; padding: 10px 15px; margin: 0 10px 10px 0; background-color: #fcfcfc; overflow: auto;}
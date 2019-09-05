5c478ff4bd6a137bf25d41c5b29874cf1a3c09de
mvl22 <martin@lucas-smith.co.uk>
2019-09-05 11:01:54 +0100

    Fix regression in e67012d5908643adf66af17950f37f0463e7bf30 which broke UCP site

--- classes/telluswhere.class.php
+++ classes/telluswhere.class.php
@@ -2425,6 +2425,7 @@ class telluswhere
 	{
 		#!# ultimateForm has multiple bugs for hidden fields when using templating; for now, standard input widgets are used and then hidden using CSS
 		$html .= "\n" . '<style type="text/css">
+			#form_latitude, #form_longitude, #form_zoom {display: none;}
 			form .latitude, form .longitude, form .zoom {display: none;}
 		</style>
 		';

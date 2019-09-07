38125a02bf0fdcf0ba752f3bd170674d6098a8c4
mvl22 <martin@lucas-smith.co.uk>
2019-09-07 19:43:17 +0100

    Avoid showing status field in popup

--- classes/telluswhere.class.php
+++ classes/telluswhere.class.php
@@ -1006,6 +1006,7 @@ class telluswhere
 		
 		# Hide internal fields
 		$popupLabels['_type'] = NULL;
+		$popupLabels['_status'] = NULL;
 		$popupLabels['iconUrl'] = NULL;
 		$popupLabels['road_name'] = NULL;
 		$popupLabels['osm_id'] = NULL;

5ce4d11f464dd65fbdc8dbdae4df03a1213a4f29
mvl22 <martin@lucas-smith.co.uk>
2019-10-01 15:37:01 +0100

    Add span to enable mobile styling to be applied to Details OK button

--- js/telluswhere.js
+++ js/telluswhere.js
@@ -703,7 +703,7 @@ var telluswhere = (function ($) {
 				if (editUrl) {
 					if (properties._status == 'initial') {
 						html += '<p class="auditbuttons' + properties.id + '">';
-						html += '<a id="auditunchanged" data-id="' + properties.id + '" href="' + editUrl + '#unchanged" class="btn waves-effect waves-light green modal-trigger" name="action">Details all OK <i class="material-icons right">check</i></a> &nbsp; ';
+						html += '<a id="auditunchanged" data-id="' + properties.id + '" href="' + editUrl + '#unchanged" class="btn waves-effect waves-light green modal-trigger" name="action"><span>Details</span> all OK <i class="material-icons right">check</i></a> &nbsp; ';
 						html += '<a href="' + editUrl + '#update" class="btn waves-effect waves-light" name="action">Edit <i class="material-icons right">build</i></a>';
 						html += '</p>';
 					} else {
